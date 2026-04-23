be4a7adddcc6b91ba0b3831d30bb31853ec0f2c0
rahmanagha <aghazad.rahman@gmail.com>
2026-04-23 16:58:05 +0400

    fix: Use __dir__ for word list path instead of relative ./
    - Replace './google-...' with File.join(__dir__, 'google-...')
    - Ensures the file is resolved relative to secret_word.rb's location
    - Fixes Errno::ENOENT when working directory is not lib/

--- lib/secret_word.rb
+++ lib/secret_word.rb
@@ -11,7 +11,8 @@ class SecretWord
   end
 
   def get_secret_word
-    words = File.readlines("./google-10000-english-words.txt", chomp: true)
+    words_path = File.join(__dir__, 'google-10000-english-words.txt')
+    words = File.readlines(words_path, chomp: true)
     words.select { |word| word.length.between?(5, 12) }.sample
   end
 
